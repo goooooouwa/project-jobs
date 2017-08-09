3f2cd17ee05068988f01ebd0cafdd277be2a244c
shadowswind <731246251@qq.com>
2017-08-09 17:16:22 +0800

    8.9_17:16

---
+++ src/js/emaiIsCorrect_InLogin.js
@@ -0,0 +1,18 @@
+function emailIsCorrect() {
+    debugger
+    var a= document.getElementById("email");
+    var b = document.getElementById("emails");
+    var d =document.getElementById("emails").getElementsByTagName("p");
+    var e =document.getElementById("login");
+    var c = /^[a-zA-Z0-9_-]+@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)+$/;
+    if(c.test(a.value)===false && d.length===0) {
+        f.disabled=true;
+        b.innerHTML+="<p>\n" +
+            "                                    <small><a href=\"/forgot/\">Email format is wrong!!!</a></small>\n" +
+            "                                </p>"
+    }
+    else if (c.test(a.value)===true){
+        b.removeChild(d[0]);
+        e.disabled=false;
+    }
+}
