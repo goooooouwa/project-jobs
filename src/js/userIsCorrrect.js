db29b99b00e533a4ccf91662bf7cbe06417ca5ad
shadowswind <731246251@qq.com>
2017-08-16 15:30:37 +0800

    8.16_15:30

--- src/js/userIsCorrrect.js
+++ src/js/userIsCorrrect.js
@@ -3,15 +3,13 @@ function userIsCorrect() {
     var b =document.getElementById("email");
     var d =document.getElementById("emails");
     var e =document.getElementById("emails").getElementsByTagName("p");
-    if (a.test(b.value)===false && e.length===0){
+     if  (a.test(b.value)||b.value===""){
+        d.removeChild(e[0]);
+    }
+    else if (a.test(b.value)===false && e.length===0){
         d.innerHTML+="<p style='color: red'>\n" +
             " <small><i>User name's format is wrong!!!</i></small>\n" +
             " </p>"
     }
-    else if  (a.test(b.value)){
-        if(e.length){
-           d.removeChild(e[0]); 
-        }
-        
-    }
+
 }
